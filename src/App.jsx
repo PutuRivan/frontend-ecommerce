c1c05d2cbbfa6a48e38aae298a47b875412b4380
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-04-01 18:55:30 +0700

    style:product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { ProductCard } from "./components/card/product-card";
 import { Navbar } from "./components/navigation/navigation-container";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <div className="grid grid-cols-3 place-items-center gap-y-5">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </>
   );
 }
